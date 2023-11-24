0c010fad0a663e90533191fb27d75dd15d759280
JKearnsl <boyarshinovn@gmail.com>
2023-11-24 21:29:30 +0300

    change field

--- src/models/tables/project_user.py
+++ src/models/tables/project_user.py
@@ -13,7 +13,7 @@ class ProjectUser(Base):
     __tablename__ = "project_users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    owner_id = Column(UUID(as_uuid=True), nullable=False)
+    user_id = Column(UUID(as_uuid=True), nullable=False)
     project_id = Column(UUID(as_uuid=True), ForeignKey("projects.id"), nullable=False)
     projects = relationship("models.tables.project.Project", back_populates="users")
 
